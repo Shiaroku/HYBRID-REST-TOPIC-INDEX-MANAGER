cd87b56bf050362a6580ffe886e730d42001866f
MEYER Sébastien <35813232+Shiaroku@users.noreply.github.com>
2020-07-30 11:34:22 +0200

    Update querying.py

--- querying.py
+++ querying.py
@@ -6,11 +6,11 @@ import json
 import numpy as np
 import os
 from dotenv import load_dotenv
+import requests
+
 
 load_dotenv(override=True)
-
 cafile_path = os.getenv('SSL_CA_PATH')
-
 index_name = "my_index"
 
 elastic = elasticsearch.Elasticsearch(['localhost'],
@@ -46,17 +46,45 @@ def get_knn_by_id(id,k):
                         })['hits']['hits']]
 
 
+def id_query(id,k):
+    knn = get_knn_by_id(id,k)
+    reference = get_name_by_id(id)
+    return """\nReference : {} \n\nNeighbors : \n\n{}""".format(os.path.basename(reference), '\n'.join(["Name : {} Score : {}".format(os.path.basename(n),s) for n,s in knn]))
+
+token = requests.request("POST", "http://127.0.0.1:8000/api/v1/auth/", data = {'username': os.getenv('DJANGO_USER'), 'password': os.getenv('DJANGO_PWD')}).json()['access']
+
+def vectoREST(text, url = "http://127.0.0.1:8000/api/v1/vectors/fr/", token = token):
+    payload = {'content': text}
+    headers = {'Authorization': 'Bearer {}'.format(token)}
+    response = requests.request("POST", url, headers=headers, data = payload)
+    return list(response.json()['dense_vector'].values())
+
+def smart_query(text,k):
+    vector = vectoREST(text) 
+    knn = [(hit['_source']['filename'], hit['_score']) for hit in elastic.search(body={"size": k, 
+                         "query": {"knn": {"vector": {"vector": vector,
+                                                      "k": k
+                                                      }
+                                          }
+                                  }
+                        })['hits']['hits']]
+    return """\nText : {} \n\nNeighbors : \n\n{}""".format(text, '\n'.join(["Name : {} Score : {}".format(os.path.basename(n),s) for n,s in knn]))
+
+
 keep_going = True
 
 while keep_going:
-    id = input("Id of reference document :")
-    k = input("Number of neighbors to search for :")
-    knn = get_knn_by_id(id,k)
-    reference = get_name_by_id(id)
-    print("""
-    Reference : {}
+    id = input("Id of reference document : ")
+    k = input("Number of neighbors to search for : ")
+    print(id_query(id,k))
+    keep_going = (input("Would you like to run another query ? (y/n): " ).lower()!="n")
 
-    Neighbors : 
-    {}
-    """.format(os.path.basename(reference), '\n'.join(["Name : {} Score : {}".format(os.path.basename(n),s) for n,s in knn])))
-    keep_going = (input("Would you like to run another query ? (y/n):" ).lower()=="y")
+keep_going = True
+
+while keep_going:
+    smart_text = input("Smart query : ")
+    k = input("Number of neighbors to search for : ")
+    print(smart_query(smart_text,k))
+    keep_going = (input("Would you like to run another query ? (y/n): " ).lower()!="n")
+
+
